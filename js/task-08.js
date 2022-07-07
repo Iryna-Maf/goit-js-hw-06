f6ca8f9ee88b7150913e2b6f6ebf949b75224d64
Iryna-Maf <100190659+Iryna-Maf@users.noreply.github.com>
2022-07-07 16:04:28 +0300

    fixed Task 8 and 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,16 @@
 'use strict';
 
-const inputEmailEl = document.querySelector('[name="email"]');
-const inputPassEl = document.querySelector('[name="password"]');
-const fornEl = document.querySelector('.login-form');
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
   const {
     elements: { email, password },
   } = event.currentTarget;
-  if (inputEmailEl.value === '' || inputPassEl.value === '') {
+  if (email.value === '' || password.value === '') {
     return alert('Please fill in all the fields');
   }
-  console.log(`Name: ${email.value}, Password: ${password.value}`);
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
 }
--- js/task-09.js
+++ js/task-09.js
@@ -4,8 +4,9 @@ const btn = document.querySelector('.change-color');
 const span = document.querySelector('.color');
 
 btn.addEventListener('click', () => {
-  document.body.style.backgroundColor = getRandomHexColor();
-  span.textContent = getRandomHexColor();
+  const colorEl = getRandomHexColor();
+  document.body.style.backgroundColor = colorEl;
+  span.textContent = colorEl;
 });
 
 function getRandomHexColor() {
